3630e077c042dd3811be037bdcad7a54dbb7c6f0
tao.chai <tao.chai@thoughtworks.com>
2021-03-01 16:17:37 +0800

    Revert "[TaoChai]: reformat: used @RestControllerAdvice

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/exception/GlobalExceptionHandler.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/exception/GlobalExceptionHandler.java
@@ -2,23 +2,23 @@ package com.thoughtworks.capacity.gtb.mvc.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import java.util.Objects;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(UserNameAlreadyExistException.class)
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     public ErrorResult handle(UserNameAlreadyExistException e) {
         return new ErrorResult(e.getMessage());
-    }
 
+    }
 
     @ExceptionHandler(UserNotfoundException.class)
     @ResponseStatus(code = HttpStatus.NOT_FOUND)
@@ -26,12 +26,12 @@ public class GlobalExceptionHandler {
         return new ErrorResult(e.getMessage());
     }
 
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     public ErrorResult handle(MethodArgumentNotValidException e) {
         String message = Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage();
         return new ErrorResult(message);
-
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
